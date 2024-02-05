f8475d5b0102ecb83d13b29d3d86ee7239b4aa5a
Sanket Kapse <sanket8397@gmail.com>
2024-02-05 11:39:55 -0700

    Updated contributor API to collaborators API

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
-from utils.contributors import get_contributors
+from utils.collaborators import get_collaborators
+
 
 if __name__ == '__main__':
     repository_owner = 'asu-cse578-s2023'
@@ -9,12 +10,12 @@ if __name__ == '__main__':
     # Set git access token in env
     access_token = os.environ.get('git_access_token')
 
-    contributors = get_contributors(
+    developers = get_collaborators(
         repository_owner, repository_name, access_token)
 
-    if contributors:
-        print(f"List of Contributors to {repository_owner}/{repository_name}:")
-        for contributor in contributors:
-            print(f"{contributor}")
+    if developers:
+        print(f"List of Developers to {repository_owner}/{repository_name}:")
+        for developer in developers:
+            print(f"{developers}")
     else:
-        print("Unable to retrieve contributors.")
+        print("Unable to retrieve developers.")
