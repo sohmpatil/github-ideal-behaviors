40d7e53f21ede8a523799f8e59a2b1e2c4ed789d
Hardik Sakhuja <hardiksakhuja421997@gmail.com>
2024-02-06 16:04:56 -0700

    implement calculating timestamp diff between consecutive commits

--- main.py
+++ main.py
@@ -29,29 +29,34 @@ def get_dev_commits():
 if __name__ == '__main__':
 
     dev_commits = get_dev_commits()
-    print(dev_commits)
+    # print(dev_commits)
 
     commits = git_utils.get_commits(
         repository_owner, repository_name, access_token)
+    # print(commits)
+    # if commits:
+    #     print(f"Commits List of {repository_owner}/{repository_name}:")
+    #     for commit in commits:
+    #         files_extension_dict = git_utils.get_changed_files(repository_owner,
+    #                                                            repository_name,
+    #                                                            commit,
+    #                                                            access_token)
+    #         print(f"Commit ID: {commit}")
+    #         print(f"Extension Counts {dict(files_extension_dict)}")
+    #         print(
+    #             f"Total changed files in the commit: {sum(files_extension_dict.values())}")
+    # else:
+    #     print("No commits found.")
 
-    if commits:
-        print(f"Commits List of {repository_owner}/{repository_name}:")
-        for commit in commits:
-            files_extension_dict = git_utils.get_changed_files(repository_owner,
-                                                               repository_name,
-                                                               commit,
-                                                               access_token)
-            print(f"Commit ID: {commit}")
-            print(f"Extension Counts {dict(files_extension_dict)}")
-            print(
-                f"Total changed files in the commit: {sum(files_extension_dict.values())}")
-    else:
-        print("No commits found.")
+    # if commits:
+    #     print(f"Commits List of {repository_owner}/{repository_name}:")
+    #     for commit in commits:
+    #         git_utils.get_number_of_new_lines(
+    #             repository_owner, repository_name, commit, access_token)
+    # else:
+    #     print("No commits found.")
+    collaborators = git_utils.get_collaborators(repository_owner, repository_name, access_token)
+    print(collaborators)
+    # for member in collaborators:
+    git_utils.fetch_consecutime_time_between_commits(repository_owner, repository_name, access_token, "sohmpatil")
 
-    if commits:
-        print(f"Commits List of {repository_owner}/{repository_name}:")
-        for commit in commits:
-            git_utils.get_number_of_new_lines(
-                repository_owner, repository_name, commit, access_token)
-    else:
-        print("No commits found.")
