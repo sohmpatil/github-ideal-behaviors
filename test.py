6a809839f4cf24ddbb499b91eb8a2f10ae4b1902
Sanket Kapse <sanket8397@gmail.com>
2024-02-25 16:55:13 -0700

    updated test code

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ repository_owner = 'sanket8397'
 repository_name = 'CSE_564_Assignment_3_4'
 
 access_token = os.environ.get('git_access_token')
-
 print(access_token)
 
 commits = git_utils.get_commits(
@@ -15,4 +14,9 @@ commits = git_utils.get_commits(
     'sanket8397'
 )
 
-print(commits)
+# print(commits)
+commit = 'a2b7614801e2a3707bbc4702b98845de5ffa0169'
+# commit = 'b49812fb1d9226f18d572849c165230a144ef4ef'
+n = git_utils.get_meaningful_lines(
+    repository_owner, repository_name, commit, access_token)
+print(n)
