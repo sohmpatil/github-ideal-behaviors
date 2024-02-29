9589300eedf102447a0f27b3c784ab8cd11359d1
Sanika Gandhe <sgandhe1@asu.edu>
2024-02-29 16:37:07 -0700

    Added data model for single commitSHA API and modified the respective functions accordingly

--- test_modifications.py
+++ test_modifications.py
@@ -20,3 +20,26 @@ for commit in commits:
     print(commit)
 print("collaborators: ", collaborators)
 
+commit = 'd05c4839a01fad8ee49102a7e531942fd6612faf'
+# commit = 'b49812fb1d9226f18d572849c165230a144ef4ef'
+n = git_utils.get_meaningful_lines(
+    repository_owner, repository_name, commit, access_token)
+print("meaningful lines: ", n)
+
+
+commit = 'd05c4839a01fad8ee49102a7e531942fd6612faf'
+# commit = 'b49812fb1d9226f18d572849c165230a144ef4ef'
+changed_files = git_utils.get_changed_files(
+    repository_owner, repository_name, commit, access_token)
+print("changed files: ", changed_files)
+
+
+additions, deletions = git_utils.get_number_of_new_lines(
+    repository_owner, repository_name, commit, access_token)
+print("Additons: ", additions)
+print("Deletion: ", deletions)
+
+
+timestamp = git_utils.get_commit_timestamp(
+    repository_owner, repository_name, commit, access_token)
+print("timestamp : ", timestamp)
