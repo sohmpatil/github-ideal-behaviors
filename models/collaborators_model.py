13bfedb11e0ccc065da599d9f95a4d0a76533ec5
Sanika Gandhe <sgandhe1@asu.edu>
2024-02-29 02:43:48 -0700

    Modified data model for collaborators API

---
+++ models/collaborators_model.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from typing import List
+
+from pydantic import BaseModel
+
+
+class Permissions(BaseModel):
+    pull: bool
+    triage: bool
+    push: bool
+    maintain: bool
+    admin: bool
+
+
+class ModelItem(BaseModel):
+    login: str
+    id: int
+    node_id: str
+    avatar_url: str
+    gravatar_id: str
+    url: str
+    html_url: str
+    followers_url: str
+    following_url: str
+    gists_url: str
+    starred_url: str
+    subscriptions_url: str
+    organizations_url: str
+    repos_url: str
+    events_url: str
+    received_events_url: str
+    type: str
+    site_admin: bool
+    permissions: Permissions
+    role_name: str
+
+
+class Model(BaseModel):
+    __root__: List[ModelItem]
