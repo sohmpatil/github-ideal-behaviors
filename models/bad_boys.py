2315175adb0ea69d23dcd58ec1fcbea57156de4a
hauntarl <sameer.mungole@gmail.com>
2024-03-01 00:12:21 -0700

    create response item

--- models/bad_boys.py
+++ models/bad_boys.py
@@ -1,4 +1,4 @@
-
+from typing import List
 from pydantic import BaseModel
 
 
@@ -6,3 +6,8 @@ class RepositoryAnalysisInput(BaseModel):
     repository_owner: str
     repository_name: str
     git_access_token: str
+
+
+class RepositoryAnalysisOutputItem(BaseModel):
+    collaborator: str
+    violated_rules: List[str]
