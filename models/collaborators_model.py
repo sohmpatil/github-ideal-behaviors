a293374c895fae95f435e66d3616de3cf04d2ef6
Sanket Kapse <sanket8397@gmail.com>
2024-03-01 00:38:55 -0700

    Collaborators redundant fields remove & list add

--- models/collaborators_model.py
+++ models/collaborators_model.py
@@ -5,36 +5,39 @@ from typing import List
 from pydantic import BaseModel
 
 
-class Permissions(BaseModel):
-    pull: bool
-    triage: bool
-    push: bool
-    maintain: bool
-    admin: bool
+# class Permissions(BaseModel):
+#     pull: bool
+#     triage: bool
+#     push: bool
+#     maintain: bool
+#     admin: bool
 
 
-class ModelItem(BaseModel):
+# class Collaborator(BaseModel):
+#     login: str
+#     id: int
+#     node_id: str
+#     avatar_url: str
+#     gravatar_id: str
+#     url: str
+#     html_url: str
+#     followers_url: str
+#     following_url: str
+#     gists_url: str
+#     starred_url: str
+#     subscriptions_url: str
+#     organizations_url: str
+#     repos_url: str
+#     events_url: str
+#     received_events_url: str
+#     type: str
+#     site_admin: bool
+#     permissions: Permissions
+#     role_name: str
+
+class Collaborator(BaseModel):
     login: str
-    id: int
-    node_id: str
-    avatar_url: str
-    gravatar_id: str
-    url: str
-    html_url: str
-    followers_url: str
-    following_url: str
-    gists_url: str
-    starred_url: str
-    subscriptions_url: str
-    organizations_url: str
-    repos_url: str
-    events_url: str
-    received_events_url: str
-    type: str
-    site_admin: bool
-    permissions: Permissions
-    role_name: str
 
 
-class CommitModel(BaseModel):
-    __root__: List[ModelItem]
+class CollaboratorsList(BaseModel):
+    collaborators: List[Collaborator]
