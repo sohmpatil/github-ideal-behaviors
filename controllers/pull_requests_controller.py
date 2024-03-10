37dbb268164338a5800dd094fe4de1bb7de51daa
Sanket Kapse <sanket8397@gmail.com>
2024-03-09 22:14:50 -0700

    pull requests controller code created

--- controllers/pull_requests_controller.py
+++ controllers/pull_requests_controller.py
@@ -1,6 +1,8 @@
 import requests
 import logging
-from pprint import pprint
+
+
+from models.pull_requests_model import PullRequestsList, PullRequests
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger("pull_requests_contoller")
@@ -10,14 +12,13 @@ def get_pull_requests(repo_owner, repo_name, access_token):
     api_url = f'https://api.github.com/repos/{repo_owner}/{repo_name}/pulls?state=all'
     headers = {'Authorization': f'token {access_token}'}
     response = requests.get(api_url, headers=headers)
+
     if response.status_code == 200:
         pr_response = response.json()
-        for pr in pr_response:
-            print(pr['id'], pr['user']['login'], pr['assignee'], pr['assignees'],
-                  pr['created_at'], pr['closed_at'])
-            if len(pr['assignees']) == 1:
-                pprint(pr)
-        return pr_response
+        pull_requests = PullRequestsList(
+            pull_requests=[PullRequests.parse_obj(pr) for pr in pr_response])
+        return pull_requests
+
     else:
         log.error(f"Error: {response.status_code}")
-        return []
+        return PullRequestsList(pull_requests=[])
