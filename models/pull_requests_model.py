37ecc8f4bdc626262888397b6a0763f7fea94fa6
Sanket Kapse <sanket8397@gmail.com>
2024-03-09 21:49:56 -0700

    data model for pull requests created

---
+++ models/pull_requests_model.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from typing import Optional, List
+
+from pydantic import BaseModel
+
+
+class PullRequests(BaseModel):
+    creator: Optional[str]
+    id: int
+    pr_assignees: Optional[List[str]]
+    created_at: str
+    closed_at: Optional[str]
+    state: str
+    merge_commit_sha: Optional[str]
+
+    @classmethod
+    def parse_obj(cls, obj):
+        user = obj.get('user', {}).get('login', '')
+        assignees = [assignee.get('login', '')
+                     for assignee in obj.get('assignees', [])]
+        return super().parse_obj({**obj, 'creator': user, 'pr_assignees': assignees})
+
+
+class PullRequestsList(BaseModel):
+    pull_requests: List[PullRequests]
