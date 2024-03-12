ce1972e76bcdfd18e2d28f48e15cdba80c783b71
sohampatil <sohampatil.2303@gmail.com>
2024-03-12 05:06:04 -0700

    Added logic for controller

--- controllers/final_data_model_controller.py
+++ controllers/final_data_model_controller.py
@@ -2,9 +2,11 @@ import logging
 
 from models.final_model import CollaboratorCommit, CollaboratorCommitList
 from models.bad_boys import RepositoryAnalysisInput
+from models.pull_requests_model import PullRequests
 from controllers.commit_details_controller import get_commit_details
 from controllers.commits_controller import get_commits
-from controllers.collaborators_controller import get_collaborators\
+from controllers.collaborators_controller import get_collaborators
+from controllers.pull_requests_controller import get_pull_requests
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger("final_data_controller")
@@ -14,6 +16,11 @@ def final_data_controller(request: RepositoryAnalysisInput) -> CollaboratorCommi
     final_data = []
 
     collaborators = get_collaborators(
+        request.repository_owner,
+        request.repository_name,
+        request.git_access_token
+    )
+    pull_requests = get_pull_requests(
         request.repository_owner,
         request.repository_name,
         request.git_access_token
@@ -37,12 +44,20 @@ def final_data_controller(request: RepositoryAnalysisInput) -> CollaboratorCommi
                 request.git_access_token
             )
             commits_details.append(commit_detail)
-
+        pr_creators = []
+        pr_assigners = []
+        for pull_request in pull_requests.pull_requests:
+            if pull_request.creator == collaborator.login:
+                pr_creators.append(pull_request)
+            elif collaborator.login in pull_request.pr_assignees:
+                pr_assigners.append(pull_request)
         collaborator_commit = CollaboratorCommit(
             collaborator=collaborator, 
-            commits=commits_details
+            commits=commits_details,
+            pr_created=pr_creators if len(pr_creators) >  0 else None,
+            pr_assigned=pr_assigners if len(pr_assigners) >  0 else None
         )
         final_data.append(collaborator_commit)
-
+ 
     final_data_model = CollaboratorCommitList(data=final_data)
     return final_data_model
--- models/final_model.py
+++ models/final_model.py
@@ -4,11 +4,14 @@ from pydantic import BaseModel
 
 from .collaborators_model import Collaborator
 from .commit_details_model import CommitDetail
+from .pull_requests_model import PullRequestsList
 
 
 class CollaboratorCommit(BaseModel):
     collaborator: Collaborator
     commits: List[CommitDetail]
+    pr_created: Optional[List[PullRequestsList]]
+    pr_assigned: Optional[List[PullRequestsList]]
 
 
 class CollaboratorCommitList(BaseModel):
