c3ceb40723419bc89e1f95b326644152026b096f
Sanika Gandhe <sgandhe1@asu.edu>
2024-03-15 15:56:05 -0700

    Added testing to test commment utils

--- tests/test_comment_utils.py
+++ tests/test_comment_utils.py
@@ -1,13 +1,68 @@
-import pytest
 from utils import comments_utils
 
-@pytest.mark.parametrize("file_name, code, expected", [
-    ("test.py", 
-    """
+def test_get_uncommented_lines_python():
+    file_name = "test.py"
+    code = """
     # This is a Python comment
     x = 5 # This is also a comment
     y = 10
-    """, 2),
-])
-def test_get_uncommented_lines(file_name, code, expected):
+    """
+    expected = 2
     assert comments_utils.get_uncommented_lines(file_name, code) == expected
+
+def test_get_uncommented_lines_java():
+    file_name = "test.java"
+    code = """
+    // This is a Java comment
+    /*
+    Multiline comment
+    */
+    int x = 5; // This is also a comment
+    int y = 10;
+    """
+    expected = 2
+    assert comments_utils.get_uncommented_lines(file_name, code) == expected
+
+def test_get_uncommented_lines_javascript():
+    file_name = "test.js"
+    code = """
+    // This is a Java  Script comment
+    /* Multiline comment
+    */
+    var x = 5; // This is also a comment
+    var y = 10;
+    """
+    expected = 2
+    assert comments_utils.get_uncommented_lines(file_name, code) == expected
+
+def test_get_uncommented_lines_c():
+    file_name = "test.c"
+    code = """
+    // This is a Java  Script comment
+    /* Multiline comment
+    */
+    int x = 5; /* Comment */
+    float y = 10.0;
+    """
+    expected = 2
+    assert comments_utils.get_uncommented_lines(file_name, code) == expected
+
+def test_get_uncommented_lines_html():
+    code = """
+    <!-- This is an HTML comment -->
+    <p>Hello, world!</p>
+    """
+    assert comments_utils.get_uncommented_lines("test.html", code) == 1
+
+
+def test_invalid_file_extension():
+    code = "dummy code"
+    assert comments_utils.get_uncommented_lines("test.txt", code) == 0
+
+def test_empty_file_extension():
+    code = "dummy code"
+    assert comments_utils.get_uncommented_lines("test", code) == 0
+
+def test_empty_code():
+    code = ""
+    assert comments_utils.get_uncommented_lines("test.py", code) == 0
