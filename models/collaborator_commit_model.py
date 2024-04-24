fea4250e535b84d1f41163f8af136120057127b1
Hardik Sakhuja <hardiksakhuja421997@gmail.com>
2024-04-23 22:17:43 -0700

    add docstring for models

--- models/collaborator_commit_model.py
+++ models/collaborator_commit_model.py
@@ -9,6 +9,17 @@ from .issues_model import Issue
 
 
 class CollaboratorCommit(BaseModel):
+    """
+    Represents a collaborator's commit details, including commits, pull requests created/assigned, and issues created/assigned.
+
+    Attributes:
+    - collaborator: Collaborator object representing the collaborator.
+    - commits: List of CommitDetail objects representing the commits made by the collaborator.
+    - pr_created: Optional list of PullRequests objects representing the pull requests created by the collaborator.
+    - pr_assigned: Optional list of PullRequests objects representing the pull requests assigned to the collaborator.
+    - issue_created: Optional list of Issue objects representing the issues created by the collaborator.
+    - issue_assigned: Optional list of Issue objects representing the issues assigned to the collaborator.
+    """
     collaborator: Collaborator
     commits: List[CommitDetail]
     pr_created: Optional[List[PullRequests]]
@@ -18,10 +29,26 @@ class CollaboratorCommit(BaseModel):
 
 
 class CollaboratorCommitList(BaseModel):
+    """
+    Represents a list of CollaboratorCommit objects.
+
+    Attributes:
+    - data: List of CollaboratorCommit objects, each representing a collaborator's commit details.
+    """
     data: List[CollaboratorCommit]
 
 
 class IndividualCollaboratorCommit(BaseModel):
+    """
+    Represents the commit details of an individual collaborator, including commits, pull requests created/assigned, and issues created/assigned.
+
+    Attributes:
+    - commits: List of CommitDetail objects representing the commits made by the collaborator.
+    - pr_created: Optional list of PullRequests objects representing the pull requests created by the collaborator.
+    - pr_assigned: Optional list of PullRequests objects representing the pull requests assigned to the collaborator.
+    - issue_created: Optional list of Issue objects representing the issues created by the collaborator.
+    - issue_assigned: Optional list of Issue objects representing the issues assigned to the collaborator.
+    """
     commits: List[CommitDetail]
     pr_created: Optional[List[PullRequests]]
     pr_assigned: Optional[List[PullRequests]]
--- models/commit_details_model.py
+++ models/commit_details_model.py
@@ -4,18 +4,43 @@ from pydantic import BaseModel
 
 
 class Author(BaseModel):
+    """
+    Represents the author of a commit.
+
+    Attributes:
+    - name: Optional name of the author.
+    - email: Optional email of the author.
+    - date: Optional date of the commit.
+    """
     name: Optional[str]
     email: Optional[str]
     date: Optional[str]
 
 
 class Committer(BaseModel):
+    """
+    Represents the committer of a commit.
+
+    Attributes:
+    - name: Optional name of the committer.
+    - email: Optional email of the committer.
+    - date: Optional date of the commit.
+    """
     name: Optional[str]
     email: Optional[str]
     date: Optional[str]
 
 
 class Commit(BaseModel):
+    """
+    Represents a commit in a GitHub repository.
+
+    Attributes:
+    - url: Optional URL to the commit.
+    - author: Optional Author object containing author details.
+    - committer: Optional Committer object containing committer details.
+    - message: Optional commit message.
+    """
     url: Optional[str]
     author: Optional[Author]
     committer: Optional[Committer]
@@ -23,12 +48,31 @@ class Commit(BaseModel):
 
 
 class Stats(BaseModel):
+    """
+    Represents the statistics of a commit, such as additions, deletions, and total changes.
+
+    Attributes:
+    - additions: Optional number of additions in the commit.
+    - deletions: Optional number of deletions in the commit.
+    - total: Optional total number of changes in the commit.
+    """    
     additions: Optional[int]
     deletions: Optional[int]
     total: Optional[int]
 
 
 class File(BaseModel):
+    """
+    Represents a file changed in a commit.
+
+    Attributes:
+    - filename: Optional name of the file.
+    - additions: Optional number of additions in the file.
+    - deletions: Optional number of deletions in the file.
+    - changes: Optional total number of changes in the file.
+    - status: Optional status of the file (e.g., added, modified, deleted).
+    - patch: Optional patch representing the changes to the file.
+    """
     filename: Optional[str]
     additions: Optional[int]
     deletions: Optional[int]
@@ -38,6 +82,16 @@ class File(BaseModel):
 
 
 class CommitDetail(BaseModel):
+    """
+    Represents detailed information about a commit, including the commit itself, stats, and files changed.
+
+    Attributes:
+    - url: Optional URL to the commit.
+    - sha: Optional SHA hash of the commit.
+    - commit: Optional Commit object containing author and committer details.
+    - stats: Optional Stats object containing additions, deletions, and total changes.
+    - files: Optional list of File objects representing the files changed in the commit.
+    """    
     url: Optional[str]
     sha: Optional[str]
     commit: Optional[Commit]
