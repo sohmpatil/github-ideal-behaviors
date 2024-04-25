a9853858a1644922a05c093026dcf31262923a03
Sanket Kapse <sanket8397@gmail.com>
2024-04-25 02:41:35 -0700

    collaborators_controller docstring added

--- controllers/collaborators_controller.py
+++ controllers/collaborators_controller.py
@@ -7,7 +7,23 @@ logging.basicConfig(level=logging.INFO)
 log = logging.getLogger("collaborators_contoller")
 
 
-def get_collaborators(repo_owner, repo_name, access_token, requests=requests) -> CollaboratorsList:
+def get_collaborators(repo_owner: str, repo_name: str, access_token: str, requests=requests) -> CollaboratorsList:
+    """
+    Retrieve a list of collaborators for a GitHub repository.
+
+    Args:
+        repo_owner (str): The owner of the GitHub repository.
+        repo_name (str): The name of the GitHub repository.
+        access_token (str): The personal access token used for authentication.
+        requests (Optional[module]): The `requests` module to use for making HTTP requests.
+            Defaults to the `requests` module if not provided explicitly.
+
+    Returns:
+        CollaboratorsList: A list of collaborators for the specified repository.
+
+    Raises:
+        None.
+    """
     api_url = f'https://api.github.com/repos/{repo_owner}/{repo_name}/collaborators'
     headers = {'Authorization': f'token {access_token}'}
     response = requests.get(api_url, headers=headers)
