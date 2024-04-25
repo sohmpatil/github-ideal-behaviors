b7d96368458770f747a3223c6d4dc7184ff09f41
smungole <smungole@asu.edu>
2024-04-25 16:58:36 -0500

    Create mock collaborator_data_model_requests

---
+++ tests/test_controllers/test_collaborator_data_model_controller.py
@@ -0,0 +1,28 @@
+import tests.test_controllers.mock_collaborator_data_model_requests as mock_requests
+
+from controllers.collaborator_data_model_controller import collaborator_individual_data_controller
+from controllers.collaborator_data_model_controller import collaborator_data_controller
+
+from models.repository_io_model import RepositoryAnalysisInput, RepositoryAnalysisIndividualInput
+from models.collaborator_commit_model import CollaboratorCommit, CollaboratorCommitList, IndividualCollaboratorCommit
+from models.collaborators_model import Collaborator
+from models.commit_details_model import CommitDetail
+from models.pull_requests_model import PullRequests
+from models.issues_model import Issue
+
+
+def test_collaborator_individual_data_controller_success():
+    pass
+
+
+def test_collaborator_individual_data_controller_empty():
+    pass
+
+
+# collaborator_data_controller
+def test_collaborator_data_controller_success():
+    pass
+
+
+def test_collaborator_data_controller_empty():
+    pass
